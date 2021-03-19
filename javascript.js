422b0c50bf7c423a6e9c646a0ee7dc1c81bb2ad8
pjdijxhoorn <pjdijxhoorn@hotmail.com>
2021-03-19 12:22:22 +0100

    added photo to the timeline. styled the title height on all the sites. fixed stretching issue on phone hero image. added javascript. and other minor styling issues.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,54 @@
 
-
-
-document.getElementById("event1").addEventListener("mouseover", mouseOver1);
-document.getElementById("event1").addEventListener("mouseout", mouseOut1);
-
-
+//--------event block-1---------//
+document.getElementById("event-1").addEventListener("mouseover", mouseOver1);
+document.getElementById("event-1").addEventListener("mouseout", mouseOut1);
 
 function mouseOver1() {
-    document.getElementById("history-explain-block").style.display = "block";
+    document.getElementById("history-explain-block-1").style.display = "block";
 }
-
 function mouseOut1() {
-    document.getElementById("history-explain-block").style.display = "none";}
+    document.getElementById("history-explain-block-1").style.display = "none";}
+
+
+//--------event block-2---------//
+document.getElementById("event-2").addEventListener("mouseover", mouseOver2);
+document.getElementById("event-2").addEventListener("mouseout", mouseOut2);
+
+function mouseOver2() {
+    document.getElementById("history-explain-block-2").style.display = "block";
+}
+function mouseOut2() {
+    document.getElementById("history-explain-block-2").style.display = "none";}
+
+
+//--------event block-3---------//
+document.getElementById("event-3").addEventListener("mouseover", mouseOver3);
+document.getElementById("event-3").addEventListener("mouseout", mouseOut3);
+
+function mouseOver3() {
+    document.getElementById("history-explain-block-3").style.display = "block";
+}
+function mouseOut3() {
+    document.getElementById("history-explain-block-3").style.display = "none";}
+
+
+//--------event block-4---------//
+document.getElementById("event-4").addEventListener("mouseover", mouseOver4);
+document.getElementById("event-4").addEventListener("mouseout", mouseOut4);
+
+function mouseOver4() {
+    document.getElementById("history-explain-block-4").style.display = "block";
+}
+function mouseOut4() {
+    document.getElementById("history-explain-block-4").style.display = "none";}
+
+
+//--------event block-5---------//
+document.getElementById("event-5").addEventListener("mouseover", mouseOver5);
+document.getElementById("event-5").addEventListener("mouseout", mouseOut5);
+
+function mouseOver5() {
+    document.getElementById("history-explain-block-5").style.display = "block";
+}
+function mouseOut5() {
+    document.getElementById("history-explain-block-5").style.display = "none";}
